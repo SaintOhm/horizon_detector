dd9ffca48141dcafd3fcfbe8c8296ebeb9a000ef
SaintOhm <165696053+SaintOhm@users.noreply.github.com>
2026-03-26 12:58:47 +0300

    Update new_project.py

--- new_project.py
+++ new_project.py
@@ -1,18 +1,29 @@
 import os
-import random
 import cv2
-import skimage.measure
+import skimage.measure #pip uninstall scikit-image
 import numpy as np
 from math import atan2, cos, sin, pi, degrees, radians
 
+# CONSTANTS
+#https://www.rapidtables.com/convert/color/rgb-to-hsv.html
+#https://www.selecolor.com/en/hsv-color-picker/
+LOWER_YELL = np.array([ 15,  50, 155])
+UPPER_YELL = np.array([ 22, 205, 255])
+LOWER_BLUE = np.array([ 90,   0, 100])
+UPPER_BLUE = np.array([125, 255, 255])
+LOWER_RED1 = np.array([  0,   0, 175])
+UPPER_RED1 = np.array([ 30,  35, 255])
+LOWER_RED2 = np.array([170,   0, 175])
+UPPER_RED2 = np.array([179,  35, 255])
 
-def show_img(img: np.array, name: str = 'N/A'):
+
+def showImg(img: np.array, name: str = 'N/A'):
     # Вывод изображения для отслеживания процесса и дебага
     cv2.namedWindow(name, cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)
     cv2.resizeWindow(name, 300, 300)
     cv2.imshow(name, img)
 
-def crop_and_scale(img: np.array):
+def cropNscale(img: np.array):
     # Сначала обрезка изображения до квадрата
     width, height = img.shape[1::-1]
     if width > height:
@@ -23,82 +34,71 @@ def crop_and_scale(img: np.array):
         img = img[int(margin):int(height - margin), :]
     
     # Теперь уменьшение
-    img = cv2.resize(img, (100, 100))
-    
-    return img
+    return cv2.resize(img, (100, 100))
 
-
-LOWER = np.array([ 90,   0,  50])
-UPPER = np.array([125, 255, 255])
-
-LOWER_BLUE = np.array([ 90,   0,  50])
-UPPER_BLUE = np.array([125, 255, 255])
-LOWER_RED1 = np.array([  0,   0, 175])
-UPPER_RED1 = np.array([ 30,  35, 255])
-LOWER_RED2 = np.array([170,   0, 175])
-UPPER_RED2 = np.array([179,  35, 255])
-
-def HSV_filter(img: np.array):
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+def HSVfilter(img: np.array):
+    img  = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    yell = cv2.inRange(img, LOWER_YELL, UPPER_YELL)
     blue = cv2.inRange(img, LOWER_BLUE, UPPER_BLUE)
     red  = cv2.bitwise_or(
         cv2.inRange(img, LOWER_RED1, UPPER_RED1),
         cv2.inRange(img, LOWER_RED2, UPPER_RED2)
     )
-    return cv2.bitwise_or(blue, red)
+    return cv2.bitwise_or(blue, red), cv2.bitwise_not(yell)
 
 #def find_pitch_and_roll(frame: np.array, debug_mode: bool = False):
 def find_pitch_and_roll(path: str, debug_mode: bool = False):
 
-    frame = crop_and_scale(cv2.imread(f'images\{path}'))
+    frame = cropNscale(cv2.imread(f'images\{path}'))
     #frame = crop_and_scale(frame)
-    bgr2gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    blue_filtered_gray = cv2.add(
-        bgr2gray,
-        cv2.inRange(
-            cv2.cvtColor(frame, cv2.COLOR_BGR2HSV),
-            LOWER, UPPER
-        )
-    )
-
-    # Отфильтровать синее небо
-    #lower = np.array([100,   0,  50])
-    #upper = np.array([125, 255, 255])
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    hsv_mask = cv2.inRange(hsv, LOWER, UPPER)
+    mask_blue, mask_fields = HSVfilter(frame)
+    blue_filtered_gray = cv2.add(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), mask_blue)
 
     blur_img = cv2.bilateralFilter(blue_filtered_gray, 9, 60, 50)
     _, bw_img = cv2.threshold(blue_filtered_gray, 250, 255, cv2.THRESH_OTSU)
+    bw_img = cv2.bitwise_and(bw_img, mask_fields)
     edges = cv2.Canny(image=blur_img, threshold1=200, threshold2=250)
 
     contours, _ = cv2.findContours(bw_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)     
     sky = sorted(contours, key=cv2.contourArea, reverse=True)[0]
-    bw_img = cv2.cvtColor(bw_img, cv2.COLOR_GRAY2BGR)
+
+    edge_points = []     # Лежат на краях изображения, поэтому точно не горизонт
+    non_edge_points = [] # Лежат на краях водоёмов, засветов и т.д.
     for i in sky:
         x, y = i[0][0], i[0][1]
         if x == 0 or x == 99 or y == 0 or y == 99:
-            bw_img[y][x] = (0, 255, 255)
+            edge_points.append(i[0])
         else:
-            bw_img[y][x] = (0, 0, 255)
+            non_edge_points.append(i[0])
+
+    avg = np.average(edge_points, axis=0) if edge_points else []
+
+    if debug_mode:
+        bw_img = cv2.cvtColor(bw_img, cv2.COLOR_GRAY2BGR)
+        if avg:
+            bw_img[int(avg[1])][int(avg[0])] = (0, 255, 255)
+        for pos in edge_points:
+            bw_img[pos[1]][pos[0]] = (0, 255, 255)
+        for pos in non_edge_points:
+            bw_img[pos[1]][pos[0]] = (0, 0, 255)
+            
+        showImg(frame, 'Original')
+        showImg(cv2.bitwise_or(mask_blue, cv2.bitwise_not(mask_fields)), 'Mask')
+        showImg(blue_filtered_gray, 'Blue filter gray')
+        showImg(blur_img, 'Blur')
+        showImg(bw_img, 'B&W')
+        showImg(edges, 'Edges')
+        cv2.imwrite(f'debug_images\{path}', cv2.bitwise_and(frame, frame, mask=mask_fields))
     
-    show_img(frame, 'Original')
-    show_img(bgr2gray, 'Gray')
-    show_img(hsv, 'HSV')
-    show_img(hsv_mask, 'HSV mask')
-    show_img(blue_filtered_gray, 'Blue filter gray')
-    show_img(blur_img, 'Blur')
-    show_img(bw_img, 'B&W')
-    show_img(edges, 'Edges')
-    #show_img(HSV_filter(frame), 'Filter')
-    cv2.imwrite(f'debug_images\{path}', cv2.bitwise_and(frame, frame, mask=cv2.bitwise_not(HSV_filter(frame))))
     return ''
 
 os.system('cls')
 for name in os.listdir('images'):
     print(name)
     print(find_pitch_and_roll(
-        #cv2.imread(f'images\{name}')
-        name
+        #cv2.imread(f'images\{name}'),
+        name,
+        debug_mode = True
     ))
     if cv2.waitKey(0) == 27: break # esc для выхода
     cv2.destroyAllWindows()
